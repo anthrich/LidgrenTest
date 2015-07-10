bd25c702f910f940842270bca2da52f9aa899b44
anthrichcaspian <anthony.richardson@caspianlearning.co.uk>
2015-07-10 18:00:40 +0100

    Init

---
+++ LidgrenTestClient/Program.cs
@@ -0,0 +1,35 @@
+﻿using Lidgren.Network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LidgrenTestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NetPeerConfiguration config = new NetPeerConfiguration("LidgrenTestServer");
+            config.AutoFlushSendQueue = false;
+            var s_client = new NetClient(config);
+
+            s_client.Start();
+            NetOutgoingMessage hail = s_client.CreateMessage("This is the hail message");
+            s_client.Connect("localhost", 14242, hail);
+
+            NetIncomingMessage msg;
+            while (true)
+            {
+                if ((msg = s_client.ReadMessage()) != null)
+                {
+                    Console.WriteLine(msg.ReadString());
+                    s_client.Recycle(msg);
+                }
+                Thread.Sleep(20);
+            }
+        }
+    }
+}
---
+++ LidgrenTestServer/Program.cs
@@ -0,0 +1,36 @@
+﻿using Lidgren.Network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LidgrenTestServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NetPeerConfiguration config = new NetPeerConfiguration("LidgrenTestServer");
+            config.Port = 14242;
+            config.MaximumConnections = 200;
+
+            NetServer server = new NetServer(config);
+
+            server.Start();
+
+            NetIncomingMessage msg;
+            while(true)
+            {
+                if((msg = server.ReadMessage()) != null)
+                {
+                    Console.WriteLine(msg.ReadString());
+                    server.Recycle(msg);
+                }
+                Thread.Sleep(20);
+            }
+        }
+    }
+}
